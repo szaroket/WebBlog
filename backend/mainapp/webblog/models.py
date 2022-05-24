90fe44b78501defaa39471edd0d33697e8d9d20b
szaroket <30178095+szaroket@users.noreply.github.com>
2022-05-24 19:17:28 +0200

    Add first version of post model

--- backend/mainapp/webblog/models.py
+++ backend/mainapp/webblog/models.py
@@ -1,3 +1,14 @@
+# A model is a Python class that subclasses django.db.models.Model,
+# in which each attribute represents a database field.
+
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    # Define attributes for blog posts
+    post_title = models.CharField(max_length=200)
+    post_author = models.CharField(max_length=20)
+    post_content = models.TextField()
+
+    def __str__(self):
+        return self.post_title
