c80adf7bc9bff4004445b3b45c4cfcd00d05b270
szaroket <30178095+szaroket@users.noreply.github.com>
2022-05-25 17:23:22 +0200

    Add new attributes to Post model

--- backend/mainapp/webblog/models.py
+++ backend/mainapp/webblog/models.py
@@ -6,9 +6,16 @@ from django.db import models
 
 class Post(models.Model):
     # Define attributes for blog posts
+    # SlugField - used for generating valid URL using obtained data
+    post_slug = models.SlugField(max_length=100)
     post_title = models.CharField(max_length=200)
     post_author = models.CharField(max_length=20)
     post_content = models.TextField()
+    # Date is set to "now" when object is first created
+    post_publish_date = models.DateTimeField(auto_now=False, auto_now_add=True)
 
     def __str__(self):
         return self.post_title
+
+    class Meta:
+        ordering = ['-post_publish_date']
