6465bf60fd3fb5119cbf1c18a6f4d7d30fa1d412
szaroket <30178095+szaroket@users.noreply.github.com>
2022-05-25 19:26:02 +0200

    Add first tests for Post unit
    Todo: Solve issue with mock time

--- backend/mainapp/webblog/tests.py
+++ backend/mainapp/webblog/tests.py
@@ -1,3 +1,32 @@
+from unittest import mock
 from django.test import TestCase
+from webblog.models import Post
+from datetime import datetime
 
-# Create your tests here.
+
+def create_model():
+    post_example = Post.objects.create(
+        post_slug="fist post",
+        post_title="First post",
+        post_author="Jan Kowalski",
+        post_content="Very important text in my first post"
+    )
+    return post_example
+
+
+class TestPostModel(TestCase):
+    def test_if_post_has_title(self):
+        post = create_model()
+        self.assertEqual(post.post_title, "First post")
+
+    # to fix: problem with mock time
+    def test_if_publish_date_is_set(self):
+        post = create_model()
+        mock_date = datetime(2022, 5, 25, 19, 10, 11, 703055)
+        with mock.patch('django.utils.timezone.now') as mock_now:
+            mock_now.return_value = mock_date
+        self.assertEqual(post.post_publish_date, mock_date)
+
+    def test_if_author_exists(self):
+        post = create_model()
+        self.assertEqual(post.post_author, "Jan Kowalski")
