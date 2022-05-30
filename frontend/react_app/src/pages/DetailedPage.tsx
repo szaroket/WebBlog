36bc50d0e1b818de4ebeedf0a1dabb770993a028
szaroket <30178095+szaroket@users.noreply.github.com>
2022-05-30 18:58:06 +0200

    Add some simple css for detailed page + getting date

--- frontend/react_app/src/pages/DetailedPage.tsx
+++ frontend/react_app/src/pages/DetailedPage.tsx
@@ -3,11 +3,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import {useLocation} from "react-router-dom";
 import {Card} from "react-bootstrap";
 import PostData from '../types/PostData';
+import "../components/cards.css"
 
 const DetailedPage = () => {
     const location = useLocation();
     const post = location.state as PostData;
-    console.log(post)
 
     // To avoid problem with rendering main page
     if (post === null) {
@@ -16,13 +16,19 @@ const DetailedPage = () => {
         )
     }
 
+    const getDateFromIsoString = (dateString: string) => {
+        const date = new Date(dateString);
+        return date.getFullYear()+'-' + (date.getMonth()+1) + '-'+date.getDate();
+    }
+
     return (
         <div className="container">
             <Card className="card h-100">
                 <Card.Body>
-                    <Card.Title className="text-center">{post.post_title}</Card.Title>
-                    <Card.Text>{post.post_content}</Card.Text>
-                    <Card.Footer className="text-muted">{post.post_author}</Card.Footer>
+                    <Card.Title className="card-title">{post.post_title}</Card.Title>
+                    <Card.Text className="card-content-detail">{post.post_content}</Card.Text>
+                    <Card.Footer
+                        className="text-muted card-footer">{post.post_author}, {getDateFromIsoString(post.post_publish_date)}</Card.Footer>
                 </Card.Body>
             </Card>
         </div>
