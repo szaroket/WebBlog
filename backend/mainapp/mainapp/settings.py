2e7d2dddbb88614aa6cd911e4d38ff86ddd964cd
szaroket <30178095+szaroket@users.noreply.github.com>
2022-05-30 19:53:47 +0200

    Use django-rest-swagger to create Swagger docs

--- backend/mainapp/mainapp/settings.py
+++ backend/mainapp/mainapp/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'webblog',
     'rest_framework',
-    'corsheaders'
+    'corsheaders',
+    'rest_framework_swagger'
 ]
 
 MIDDLEWARE = [
--- backend/mainapp/webblog/urls.py
+++ backend/mainapp/webblog/urls.py
@@ -1,11 +1,20 @@
-from django.urls import include, path
+from django.urls import path
 from webblog import views
-
+from rest_framework.schemas import get_schema_view
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('posts/', views.ListOfAllPosts.as_view()),
     path('post/<int:pk>/', views.PostDetails.as_view()),
     path('post/delete/<int:pk>', views.DeletePost.as_view()),
     path('post/create/', views.CreatePost.as_view()),
-    path('post/update/<int:pk>', views.UpdatePost.as_view())
+    path('post/update/<int:pk>', views.UpdatePost.as_view()),
+    path('openapi/', get_schema_view(
+        title="WebBlog",
+        description="API for WebBlog project"
+    ), name="openapi-schema"),
+    path('docs/', TemplateView.as_view(
+        template_name='./documentation.html',
+        extra_context={'schema_url': 'openapi-schema'}
+    ), name='swagger-ui')
 ]
