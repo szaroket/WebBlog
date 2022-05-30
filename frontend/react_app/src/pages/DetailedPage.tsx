71a3dcada5b4936417f0b7a85c55c6496cbd5955
szaroket <30178095+szaroket@users.noreply.github.com>
2022-05-30 19:02:46 +0200

    Put detailed card in col

--- frontend/react_app/src/pages/DetailedPage.tsx
+++ frontend/react_app/src/pages/DetailedPage.tsx
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {useLocation} from "react-router-dom";
-import {Card} from "react-bootstrap";
+import {Card, Col} from "react-bootstrap";
 import PostData from '../types/PostData';
 import "../components/cards.css"
 
@@ -18,19 +18,22 @@ const DetailedPage = () => {
 
     const getDateFromIsoString = (dateString: string) => {
         const date = new Date(dateString);
-        return date.getFullYear()+'-' + (date.getMonth()+1) + '-'+date.getDate();
+        return date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate();
     }
 
     return (
         <div className="container">
-            <Card className="card h-100">
-                <Card.Body>
-                    <Card.Title className="card-title">{post.post_title}</Card.Title>
-                    <Card.Text className="card-content-detail">{post.post_content}</Card.Text>
-                    <Card.Footer
-                        className="text-muted card-footer">{post.post_author}, {getDateFromIsoString(post.post_publish_date)}</Card.Footer>
-                </Card.Body>
-            </Card>
+            <Col className="col-lg-12">
+                <Card className="card h-100" style={{borderRadius: "15px"}}>
+                    <Card.Body>
+                        <Card.Title className="card-title">{post.post_title}</Card.Title>
+                        <Card.Text className="card-content-detail">{post.post_content}</Card.Text>
+                        <Card.Footer
+                            className="text-muted card-footer">{post.post_author}, {getDateFromIsoString(post.post_publish_date)}</Card.Footer>
+                    </Card.Body>
+                </Card>
+            </Col>
+
         </div>
     )
 }
